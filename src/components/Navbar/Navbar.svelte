f215a9306e0f692a78bfd59b702627560aff3941
Leonard Freimuth <leonard.freimuth@gmail.com>
2021-01-16 14:04:54 +0100

    center navbar content on large screens

--- src/components/Navbar/Navbar.svelte
+++ src/components/Navbar/Navbar.svelte
@@ -9,29 +9,31 @@
   role="navigation"
   aria-label="main navigation"
 >
-  <div class="navbar-brand">
-    <a class="navbar-item has-text-weight-bold" href="/">SveTiBuTe</a>
+  <div class="container">
+    <div class="navbar-brand">
+      <a class="navbar-item has-text-weight-bold" href="/">SveTiBuTe</a>
 
-    <span
-      class:is-active={$isActive}
-      on:click={toggleActive}
-      role="button"
-      class="navbar-burger"
-      aria-label="menu"
-      aria-expanded="false">
-      <span aria-hidden="true" />
-      <span aria-hidden="true" />
-      <span aria-hidden="true" />
-    </span>
-  </div>
-  <div class="navbar-menu " class:is-active={$isActive}>
-    <div class="navbar-start">
-      <NavbarLink path="/props">Props</NavbarLink>
-      <NavbarLink path="/yap">YAP</NavbarLink>
+      <span
+        class:is-active={$isActive}
+        on:click={toggleActive}
+        role="button"
+        class="navbar-burger"
+        aria-label="menu"
+        aria-expanded="false">
+        <span aria-hidden="true" />
+        <span aria-hidden="true" />
+        <span aria-hidden="true" />
+      </span>
     </div>
+    <div class="navbar-menu " class:is-active={$isActive}>
+      <div class="navbar-start">
+        <NavbarLink path="/props">Props</NavbarLink>
+        <NavbarLink path="/yap">YAP</NavbarLink>
+      </div>
 
-    <div class="navbar-end">
-      <!-- navbar items -->
+      <div class="navbar-end">
+        <!-- navbar items -->
+      </div>
     </div>
   </div>
 </nav>
